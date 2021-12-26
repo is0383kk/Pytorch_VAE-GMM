3bf295836b0e6403c36b9be39a628e0f615f6ac9
is0383kk <is0383kk@gmail.com>
2021-12-27 00:44:49 +0900

    Bug fixes

--- vae_module.py
+++ vae_module.py
@@ -15,24 +15,24 @@ from tool import visualize_ls, sample, get_param
 
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-z_dim=12
+x_dim=12 # dimention of latent variable
 class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
 
         self.fc1 = nn.Linear(784, 256)
-        self.fc21 = nn.Linear(256, z_dim)
-        self.fc22 = nn.Linear(256, z_dim)
-        self.fc3 = nn.Linear(z_dim, 256)
+        self.fc21 = nn.Linear(256, x_dim)
+        self.fc22 = nn.Linear(256, x_dim)
+        self.fc3 = nn.Linear(x_dim, 256)
         self.fc4 = nn.Linear(256, 784)
         # 事前分布のパラメータN(0,I)で初期化
-        self.prior_var = nn.Parameter(torch.Tensor(1, z_dim).float().fill_(1.0))
+        self.prior_var = nn.Parameter(torch.Tensor(1, x_dim).float().fill_(1.0))
         self.prior_logvar = nn.Parameter(self.prior_var.log())
         self.prior_var.requires_grad = False
         self.prior_logvar.requires_grad = False
 
-    def encode(self, x):
-        h1 = F.relu(self.fc1(x))
+    def encode(self, o_d):
+        h1 = F.relu(self.fc1(o_d))
         return self.fc21(h1), self.fc22(h1)
 
     def reparameterize(self, mu, logvar):
@@ -40,18 +40,18 @@ class VAE(nn.Module):
         eps = torch.randn_like(std)
         return mu + eps*std
 
-    def decode(self, z):
-        h3 = F.relu(self.fc3(z))
+    def decode(self, x_d):
+        h3 = F.relu(self.fc3(x_d))
         return torch.sigmoid(self.fc4(h3))
 
-    def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar, z
+    def forward(self, o_d):
+        mu, logvar = self.encode(o_d.view(-1, 784))
+        x_d = self.reparameterize(mu, logvar)
+        return self.decode(x_d), mu, logvar, x_d
      
     # Reconstruction + KL divergence losses summed over all elements and batch
-    def loss_function(self, recon_x, x, en_mu, en_logvar, gmm_mu, gmm_var, iteration):
-        BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    def loss_function(self, recon_x, o_d, en_mu, en_logvar, gmm_mu, gmm_var, iteration):
+        BCE = F.binary_cross_entropy(recon_x, o_d.view(-1, 784), reduction='sum')
         beta = 5.0
         # see Appendix B from VAE paper:
         # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
@@ -73,13 +73,13 @@ class VAE(nn.Module):
             diff = en_mu - prior_mu # μ_１ - μ_0
             diff_term = diff *diff / prior_var # (μ_1 - μ_0)(μ_1 - μ_0)/Σ_1
             logvar_division = prior_logvar - en_logvar # log|Σ_1| - log|Σ_0| = log(|Σ_1|/|Σ_2|)
-            KLD = 0.5 * ((var_division + diff_term + logvar_division).sum(1) - z_dim)
+            KLD = 0.5 * ((var_division + diff_term + logvar_division).sum(1) - x_dim)
         else:
             KLD = -0.5 * torch.sum(1 + en_logvar - en_mu.pow(2) - en_logvar.exp())
         return BCE + KLD
 
 def train(iteration, gmm_mu, gmm_var, epoch, train_loader, all_loader, model_dir="./vae_gmm"):
-    prior_mean = torch.Tensor(len(train_loader), z_dim).float().fill_(0.0) # 最初のVAEの事前分布の\mu
+    prior_mean = torch.Tensor(len(train_loader), x_dim).float().fill_(0.0) # 最初のVAEの事前分布の\mu
     model = VAE().to(device)
     print("VAE Training Start")
     #loss_list = []
@@ -98,8 +98,8 @@ def train(iteration, gmm_mu, gmm_var, epoch, train_loader, all_loader, model_dir
         for batch_idx, (data, _) in enumerate(train_loader):
             data = data.to(device)
             optimizer.zero_grad()
-            recon_batch, mu, logvar, z = model(data)
-            if iteration==0: # 最初の学習
+            recon_batch, mu, logvar, x_d = model(data)
+            if iteration==0:
                 loss = model.loss_function(recon_batch, data, mu, logvar, gmm_mu=None, gmm_var=None, iteration=iteration)
             else:
                 loss = model.loss_function(recon_batch, data, mu, logvar, gmm_mu[batch_idx], gmm_var[batch_idx], iteration=iteration)
@@ -111,15 +111,17 @@ def train(iteration, gmm_mu, gmm_var, epoch, train_loader, all_loader, model_dir
             print('====> Epoch: {} Average loss: {:.4f}'.format(
             i+1, train_loss / len(train_loader.dataset)))
             
-        loss_list[i] = train_loss / len(train_loader.dataset)
-    #グラフ処理
+        loss_list[i] = -(train_loss / len(train_loader.dataset))
+    
     plt.figure()
-    plt.plot(range(0,epoch), loss_list, color="blue", label="loss")
-    plt.xlabel('epoch')
-    plt.ylabel('VAE_loss')
+    plt.plot(range(0,epoch), loss_list, color="blue", label="ELBO")
+    if iteration!=0: 
+        loss_0 = np.load(model_dir+'/npy/loss_0.npy')
+        plt.plot(range(0,epoch), loss_0, color="red", label="ELBO_I0")
+    plt.xlabel('epoch'); plt.ylabel('ELBO'); plt.legend(loc='lower right')
     plt.savefig(model_dir+'/graph/vae_loss_'+str(iteration)+'.png')
     plt.close()
-    #np.save('./loss_list.npy', np.array(loss_list))
+    
     torch.save(model.state_dict(), model_dir+"/pth/vae_"+str(iteration)+".pth")
     x_d, label = send_all_z(iteration=iteration, all_loader=all_loader, model_dir=model_dir)
     return x_d, label, loss_list
@@ -130,7 +132,7 @@ def decode(iteration, decode_k, sample_num, model_dir="./vae_gmm"):
     model.load_state_dict(torch.load(str(model_dir)+"/pth/vae_"+str(iteration)+".pth"))
     model.eval()
     mu_gmm_kd, lambda_gmm_kdd, pi_gmm_k = get_param(iteration, model_dir=model_dir)
-    manual_sample, random_sample = sample(iteration=iteration, z_dim=z_dim, 
+    manual_sample, random_sample = sample(iteration=iteration, x_dim=x_dim, 
                      mu_gmm=mu_gmm_kd, lambda_gmm=lambda_gmm_kdd, 
                      sample_num=sample_num, sample_k=decode_k, model_dir=model_dir
                      )
@@ -151,15 +153,9 @@ def plot_latent(iteration, all_loader, model_dir="./vae_gmm"): # VAEの潜在空
     model.eval()
     for batch_idx, (data, label) in enumerate(all_loader):
         data = data.to(device)
-        recon_batch, mu, logvar, z = model(data)
-        z = z.cpu()
-        #if classes is not None:
-        #    ari, result = calc_ari(classes, label.numpy())
-        #    print(f"ari:{round(ari,3)}")
-        #    ari = round(ari,3)
-        #np.savetxt('z5000.txt', z.detach().numpy())
-        #np.savetxt('label5000.txt', label.detach().numpy())
-        visualize_ls(iteration, z.detach().numpy(), label, model_dir)
+        recon_batch, mu, logvar, x_d = model(data)
+        x_d = x_d.cpu()
+        visualize_ls(iteration, x_d.detach().numpy(), label, model_dir)
         break
 
 def test(epoch):
@@ -181,13 +177,13 @@ def test(epoch):
                 save_image(comparison.cpu(),
                          'image/recon_' + str(epoch) + '.png', nrow=n)
 
-def send_all_z(iteration, all_loader, model_dir="./vae_gmm"): # gmmにvaeの潜在空間を送るメソッド
+def send_all_z(iteration, all_loader, model_dir="./vae_gmm"): # method to send vae latent variable:x_d to gmm
     model = VAE().to(device)
     model.load_state_dict(torch.load(model_dir+"/pth/vae_"+str(iteration)+".pth"))
     model.eval()
     for batch_idx, (data, label) in enumerate(all_loader):
         data = data.to(device)
-        recon_batch, mu, logvar, z = model(data)
-        z = z.cpu()
+        recon_batch, mu, logvar, x_d = model(data)
+        x_d = x_d.cpu()
         label = label.cpu()
-    return z.detach().numpy(), label.detach().numpy()
+    return x_d.detach().numpy(), label.detach().numpy()
